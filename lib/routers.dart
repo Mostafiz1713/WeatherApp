cad0d0760aac132d5cf53063cf3815ae822932c9
Md Mostafizur Rahman <mostafizur@aapbd.com>
2020-12-29 22:20:10 +0600

    first commit

---
+++ lib/routers.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Router {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => );
+      case '/feed':
+        return MaterialPageRoute(builder: (_) => );
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+              body: Center(
+                  child: Text('No route defined for ${settings.name}')),
+            ));
+    }
+  }
+}
